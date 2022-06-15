81d398baeb3e73abc664187cb02ff3f561df5de1
datmax <max.datsun@student.univaq.it>
2022-06-15 20:22:33 +0200

    ok

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,10 +1,9 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment, useState } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
-import { CheckIcon } from '@heroicons/react/outline'
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { CheckIcon } from "@heroicons/react/outline";
 
-export default function Example({open, setOpen}) {
-
+export default function Example({ open, setOpen }) {
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog as="div" className="relative z-10" onClose={setOpen}>
@@ -15,8 +14,7 @@ export default function Example({open, setOpen}) {
           enterTo="opacity-100"
           leave="ease-in duration-200"
           leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
+          leaveTo="opacity-0">
           <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
         </Transition.Child>
 
@@ -29,17 +27,22 @@ export default function Example({open, setOpen}) {
               enterTo="opacity-100 translate-y-0 sm:scale-100"
               leave="ease-in duration-200"
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            >
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
               <Dialog.Panel className="relative bg-black rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-sm sm:w-full sm:p-6">
                 <div>
-                
                   <div className="mt-3 text-center sm:mt-5">
-                    <Dialog.Title as="h3" className="text-3xl py-2 leading-6 font-medium text-themegreen">
-                      Mint successful!
+                    <Dialog.Title
+                      as="h3"
+                      className="text-3xl py-2 leading-6 font-medium text-themegreen">
+                      Mint successful! Check your NFT on{" "}
+                      <a href="https://trove.treasure.lol/">Trove.</a>
                     </Dialog.Title>
                     <div className="mt-2">
-                      <img src="prereveal.png" alt=""  className='rounded-lg my-2'/>
+                      <img
+                        src="prereveal.png"
+                        alt=""
+                        className="rounded-lg my-2"
+                      />
                     </div>
                   </div>
                 </div>
@@ -47,8 +50,7 @@ export default function Example({open, setOpen}) {
                   <button
                     type="button"
                     className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-purple-800 text-base font-medium text-white  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
-                    onClick={() => setOpen(false)}
-                  >
+                    onClick={() => setOpen(false)}>
                     Go back
                   </button>
                 </div>
@@ -58,5 +60,5 @@ export default function Example({open, setOpen}) {
         </div>
       </Dialog>
     </Transition.Root>
-  )
+  );
 }
